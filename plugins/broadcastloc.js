1d2861da0d109d8bb032e0c2173ff3d5ddacaa3e
GW VERDI <107461433+XziroID@users.noreply.github.com>
2022-06-25 21:07:05 +0700

    Update broadcastloc.js

--- plugins/broadcastloc.js
+++ plugins/broadcastloc.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
     let groups = Object.keys(await conn.groupFetchAllParticipating())
-    m.reply(`_mengirim pesan siaran ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
+    m.reply(`_loading wait in minute...._ ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
     for (let id of groups) {
-        let bg = 'https://telegra.ph/file/5ace1bc56f072d311ac44.jpg'
+        let bg = 'https://telegra.ph/file/a352c6bb8895f9df1d5ff.mp4'
         await conn.delay(1500)
         await conn.sendButtonLoc(id, await (await fetch(bg)).buffer(), text, wm, 'broadcast', 'broadcast')
     }
